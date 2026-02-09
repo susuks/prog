edc2301c8fd2835c11231e355bc33b2bb0de7457
Nicolai <nicolai.teslenco@gmail.com>
2026-02-09 15:12:57 -0400

    mod limpando cache gitignore

--- Exec.bat
+++ Exec.bat
@@ -10,11 +10,8 @@ echo Iniciando os 3 robos em janelas separadas...
 :: 1. Inicia o Monitor de Vendas (Texto)
 start "1. MONITOR" cmd /k "node monitor.js"
 
-:: 2. Inicia o Coletor Python (Intranet)
+:: 2. Inicia o Coletor Python (Intranet e Planilhas Google)
 start "2. COLETOR" cmd /k "python coleta_dados.py"
-
-:: 3. Inicia o Carteiro de Mídia
-start "3. ENCAMINHADOR" cmd /k "node encaminhar_midia.js"
 
 echo.
 echo Tudo iniciado! Pode minimizar esta janela.
