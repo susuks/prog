c10eaddcceb9d8aa72793ff0805582cd13eb75d3
nicolai <nicolai.teslenco@gmail.com>
2026-04-07 11:46:47 -0400

    Pré Alpha 1

--- Exec_1.bat
+++ Exec_1.bat
@@ -4,8 +4,6 @@ cd /d "%~dp0main prog"
 echo ==========================================
 echo   INICIANDO SISTEMA DE AUTOMACAO TOTAL
 echo ==========================================
-echo.
-echo Iniciando os 3 robos em janelas separadas...
 
 :: 1. Inicia o Monitor de Vendas (Texto)
 start "1. MONITOR" cmd /k "node monitor.js"
@@ -16,6 +14,5 @@ start "2. COLETOR" cmd /k "python coleta_dados.py"
 :: 3. Inicia o Carteiro de Mídia
 start "3. ENCAMINHADOR" cmd /k "node encaminhar_midia.js"
 
-echo.
 echo Tudo iniciado! Pode minimizar esta janela.
 pause
